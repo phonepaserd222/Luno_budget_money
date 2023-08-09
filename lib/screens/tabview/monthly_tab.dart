711ec7c2b22b549af6e406b581a1949b70e105e8
phonepaserd222 <phonepaserdsimphavong@gmail.com>
2023-08-09 17:07:02 +0700

    .

--- lib/screens/tabview/monthly_tab.dart
+++ lib/screens/tabview/monthly_tab.dart
@@ -8,14 +8,57 @@ class MonthlyTab extends StatefulWidget {
 }
 
 class _MonthlyTabState extends State<MonthlyTab> {
+  final controllerName = TextEditingController();
+  final controllerAge = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(labelText: 'name'),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          TextFormField(
+            controller: controllerName,
+            decoration: InputDecoration(
+              labelText: 'name',
+              // prefixIcon: const Icon(Icons.calculate),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: const BorderSide(
+                    width: 1, color: Color.fromRGBO(112, 20, 204, 1)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                    width: 1, color: Color.fromRGBO(112, 20, 204, 1)),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            keyboardType: TextInputType.text,
+          ),
+          const SizedBox(height: 10),
+          // 2
+          TextFormField(
+            controller: controllerAge,
+            decoration: InputDecoration(
+              labelText: 'age',
+              // prefixIcon: const Icon(Icons.calculate),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: const BorderSide(
+                    width: 1, color: Color.fromRGBO(112, 20, 204, 1)),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                    width: 1, color: Color.fromRGBO(112, 20, 204, 1)),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            keyboardType: TextInputType.text,
+          ),
+          const SizedBox(height: 10),
+          // 3
+          ElevatedButton(onPressed: () {}, child: Text('Save'))
+        ],
+      ),
     );
   }
 }
